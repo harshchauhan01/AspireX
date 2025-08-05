649797ccda49c209e3e4c45d05fa10d8f3b14c3f
chauh_harsh <chauhanharsh226@gmail.com>
2025-08-05 17:16:47 +0530

    Fixed minute busg

--- frontend/src/Student/pages/MentorProfile.jsx
+++ frontend/src/Student/pages/MentorProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import styles from './css/MentorProfile.module.css';
+import './css/MentorProfile.module.css';
 import API from '../../BackendConn/api';
 import upiQr from '../../assets/qrcode.jpeg';
 import coverImg from '../../assets/mentoring.webp';
